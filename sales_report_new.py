5f2363c4cef0ac9df1e9aa2baf599dc6922cdea0
Yadi Wang <wangydtju@gmail.com>
2022-02-12 07:32:14 +0000

    add comments on sales_report.py and make improvements with a new file sales_report_new

---
+++ sales_report_new.py
@@ -0,0 +1,32 @@
+"""Generate sales report showing total melons each salesperson sold."""
+
+
+salespeople = []         # create a blank list for sales person
+# melons_sold = []            # create a blank list for melons sold
+melons_sold = {}
+f = open('sales-report.txt')
+for line in f:
+    line = line.rstrip()
+    entries = line.split('|')  # split each line and make a list for each entry
+
+    salesperson = entries[0]    # name of each sales person is the first word 
+    melons = int(entries[2])      # number of melon sold is the third word in the line and convert it to integer
+
+#     if salesperson in salespeople:   # if the name already in the salespeople list
+#         position = salespeople.index(salesperson)    # find-out the index position of the person in the salespeople list 
+
+#         melons_sold[position] += melons    # in the same index of melons_sold list, add the number of melons to the existing number
+#     else:                     # if the person is the first time show up, add the name and number of lemons into both lists
+    salespeople.append(salesperson)    
+#         melons_sold.append(melons)
+    # melons_sold[salesperson] = melons
+
+for salesperson in salespeople:
+    melons_sold[salesperson] = melons_sold.get(salesperson, 0) + melons
+
+for salesperson, melons in melons_sold.items():
+    print(f'{salesperson} sold {melons} melons')
+
+
+# for i in range(len(salespeople)):     # each person in the list print the name and numbet of melons 
+#     print(f'{salespeople[i]} sold {melons_sold[i]} melons')
